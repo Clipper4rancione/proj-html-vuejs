b5f402e4f18650615ba4039562f502034aef041d
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-11-24 17:58:59 +0100

    [add] images

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,15 +6,54 @@ export default {
 
 <template>
     <header>
-        <div class="container">
-            H
-            <!-- Logo -->
-            <!-- Menu navigazione dinamico -->
+        <!-- Logo -->
+        <!-- Menu navigazione dinamico -->
+        <div class="h-wrapper">
+            <div class="logo-area">
+                <img src="../assets/img/medical_logo_1x_dark.png" alt="">
+            </div>
+
+            <div class="nav-area">
+                <ul>
+                    <li><a href="#">NAV</a></li>
+                    <li><a href="#">NAV</a></li>
+                    <li><a href="#">NAV</a></li>
+                    <li><a href="#">NAV</a></li>
+                    <li><a href="#">NAV</a></li>
+                </ul>
+            </div>
         </div>
     </header>
 </template>
 
 
 <style lang="scss" scoped>
+    .h-wrapper{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        padding: 18px;
+
+
+        .logo-area{
+            height: 100%;
+            width: 170px;
+
+            img{
+                width: 100%;
+                vertical-align: middle;
+
+            }
+        }
+        ul{
+            list-style: none;
+            display: flex;
+
+            li{
+                padding-left: 25px;
+            }
+        }
+    }
 
 </style>
