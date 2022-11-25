98fc26b1ad2338fce5d0b36d9b57549acb386c68
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-11-25 19:19:59 +0100

    changes to img path

--- src/data/store.js
+++ src/data/store.js
@@ -5,24 +5,24 @@ export const store = reactive({
     /*INFO CARD OBJECT*/
     infoCard:[
         {
-            img: './assets/img/icon-1.png',
-            caption: 'Same Day Appointments',
-            comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+            img     : 'icon-1.png',
+            caption : 'Same Day Appointments',
+            comment : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
         },
         {
-            img: './assets/img/icon-2.png',
-            caption: 'World class facilities',
-            comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+            img     : 'icon-2.png',
+            caption : 'World class facilities',
+            comment : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
         },
         {
-            img: './assets/img/icon-3.png',
-            caption: 'expert doctors',
-            comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+            img     : 'icon-3.png',
+            caption : 'expert doctors',
+            comment : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
         },
         {
-            img: './assets/img/icon-4.png',
-            caption: 'complementary therapies',
-            comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+            img     : 'icon-4.png',
+            caption : 'complementary therapies',
+            comment : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
         },
     ]
     /*/INFO CARD OBJECT*/
