59e27c5a0f9f5f7f5b65d6674d493ac8b20753b4
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-11-25 18:02:08 +0100

    [add] store.js and first object

---
+++ src/data/store.js
@@ -0,0 +1,30 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+
+    /*INFO CARD OBJECT*/
+    infoCard:[
+        {
+            infoImg: 'icon-1.png',
+            caption: 'Same Day Appointments',
+            comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, quas. Impedit eveniet'
+        },
+        {
+            infoImg: 'icon-2.png',
+            caption: 'World class facilities',
+            comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, quas. Impedit eveniet'
+        },
+        {
+            infoImg: 'icon-3.png',
+            caption: 'expert doctors',
+            comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, quas. Impedit eveniet'
+        },
+        {
+            infoImg: 'icon-4.png',
+            caption: 'complementary therapies',
+            comment: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione, quas. Impedit eveniet'
+        },
+    ]
+    /*/INFO CARD OBJECT*/
+
+})
