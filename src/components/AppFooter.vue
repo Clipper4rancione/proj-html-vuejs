f769ef7673fef1c812ab72e40fd14f9e06c7af73
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-11-28 07:55:57 +0100

    [add] center and right section

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,12 @@
 <script>
+import { store } from '../data/store.js';
 export default {
-    name: 'AppFooter'
+    name: 'AppFooter',
+    data(){
+        return{
+            store,
+        }
+    }
 }
 </script>
 
@@ -32,16 +38,38 @@ export default {
 
 
                     <!-- Center Section -->
-                    <div class="col-auto center-section">
-                        c
-                        <!-- h2 -->
+                    <div class="col-4 center-section">
+                        <div class="container">
+                        <h6>CONTACT INFORMATION</h6>
                         <!-- List -->
+                        <ul>
+                            <li>
+                                <i class="fa-solid fa-location-dot"></i>
+                                121 King street, London, United Kingdom
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-phone"></i>
+                                1.800.458.566
+                            </li>
+                        </ul>
+
+                        </div>
                     </div>
                     <!-- /Center Section -->
                     
                     <!-- Right Section -->
-                    <div class="col-auto right-section">
-                        r
+                    <div class="col-4 right-section">
+                        <div class="container">
+                            <h6>Medical Departments</h6>
+
+                            <ul>
+                                <li v-for="(item, index) in store.footerList" 
+                                    :key="index">
+                                        <a :href="item.link"></a>
+                                        {{item.name}}
+                                </li>
+                            </ul>
+                        </div>
                         <!-- h2 -->
                         <!-- List -->
                         <!-- Button CTA -->
@@ -63,6 +91,10 @@ export default {
         padding: 20px;
         display: flex;
 
+        h6{
+            text-transform: uppercase;
+            margin-bottom: 35px;
+        }
         .left-section {
 
             img{
@@ -83,12 +115,48 @@ export default {
             .link-area{
                 margin-top: 10px;
 
+                a{
+                    color: black;
+                }
+
                 i{
                     cursor: pointer;
                     font-size: 2rem;
                     margin-top: 35px;
                     margin-right: 15px;
                 }
+            }
+        }
+        .center-section{
+            li{
+                margin-top: 20px;
+                
+                i{
+                    font-size: 1.2rem;
+                    margin-right: 5px;
+                    color: #3aafbf;
+                }
+            }
+        }
+        .right-section{
+
+            li{
+                line-height: 35px;
+                cursor: pointer;
+            }
+            li:before{
+                content: '\f054';
+                font-family: 'Font Awesome 6 Free';
+                font-weight: 800;
+                margin-right: 10px;
+
+                padding: 10px;
+                line-height: 5px;
+
+                font-size: .6rem;
+                border-radius: 50%;
+                background-color: #3aafbf;
+                color: white;
             }
         }
     }
